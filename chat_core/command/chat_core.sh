4ece7d0bcc142fd6b2dcfdcb412e148c8a24fc50
JaWeee <dohoanggiahuy310703@gmail.com>
2024-07-30 22:17:41 +0700

    update gitignore

--- chat_core/command/chat_core.sh
+++ chat_core/command/chat_core.sh
@@ -6,14 +6,14 @@ export PYTHONPATH="$ROOT_DIR"
 
 python3 chat_core/chatbot.py \
     --question "What are Coca-Cola\u2019s primary goals in its sustainability initiatives, and how are they measured?" \
-    --compressor_type 3 \
+    --compressor_type 1 \
     --db_path "./rag_core/database/cocacola/faiss_db" \
-    --user_id "123" \
-    --conversation_id "1"
+    --user_id "admin" \
+    --conversation_id "2"
 
 python3 chat_core/chatbot.py \
     --question "Can you summarize your history reponse in 1 sentence?" \
-    --compressor_type 3 \
+    --compressor_type 1 \
     --db_path "./rag_core/database/cocacola/faiss_db" \
-    --user_id "123" \
-    --conversation_id "1"
+    --user_id "admin" \
+    --conversation_id "2"
